aa57b5025d4729fc636475186dcd629560eec3eb
Hayk <hayktunyan1994@gmail.com>
2022-03-24 18:25:17 +0400

    update firabase app

--- src/components/header.js
+++ src/components/header.js
@@ -24,7 +24,7 @@ const HeaderComponent = () => {
               <MenuIcon />
             </IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Inomma
+              Firebase App
             </Typography>
           </Toolbar>
         </AppBar>
